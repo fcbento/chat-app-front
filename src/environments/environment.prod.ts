b257f1cd97ec11b718a4f5a3f6be8c6bfb7a8714
Felipe <felipe.16costa@gmail.com>
2020-08-07 15:37:04 -0300

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  SERVER: 'https://chat-001-dev.azurewebsites.net',
-  API_URL: 'https://chat-001-dev.azurewebsites.net/api/v1',
+  SERVER: 'https://chat-back.azurewebsites.net',
+  API_URL: 'https://chat-back.azurewebsites.net/api/v1',
   API_COUNTRIES: 'https://restcountries.eu'
 };
