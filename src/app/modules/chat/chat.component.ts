fad7729c8c6982946475eee76ad4e6f4c4e413b0
Felipe <felipe.16costa@gmail.com>
2020-08-08 10:57:20 -0300

    test

--- src/app/modules/chat/chat.component.ts
+++ src/app/modules/chat/chat.component.ts
@@ -69,7 +69,7 @@ export class ChatComponent implements OnInit, OnDestroy {
     this.socket.on('newMessage', (message) => {
 
       if (message.text.includes('has joined')) {
-        this.notificationService.userOn(this.message.user)
+        this.notificationService.userOn(message.from.user)
       } else {
         this.vc.createEmbeddedView(this.template,
           {
