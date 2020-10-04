a0d2f9a9b706b8297ed8a8d44ce643551cec2f84
Felie Bento <felipe.16costa@gmail.com>
2020-10-04 00:13:37 -0300

    Validate youtube link

--- src/app/shared/components/modal/content/youtube-link/youtube-link.component.ts
+++ src/app/shared/components/modal/content/youtube-link/youtube-link.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { toJSDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-calendar';
 
 @Component({
   selector: 'app-youtube-link',
@@ -9,6 +10,7 @@ export class YoutubeLinkComponent implements OnInit {
 
   @Output() youtubeLink = new EventEmitter();
   link: any;
+  isYoutubeLink: boolean = false;
 
   constructor() { }
 
@@ -17,6 +19,23 @@ export class YoutubeLinkComponent implements OnInit {
 
   sendYoutube() {
     this.youtubeLink.emit(this.link)
+  }
+
+  validateYoutubeLink() {
+
+    if (this.validate()) {
+      this.isYoutubeLink = true;
+    } else {
+      this.isYoutubeLink = false;
+    }
+  }
+
+  validate() {
+    let p = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+    if (this.link.match(p)) {
+      return this.link.match(p)[1];
+    }
+    return false;
   }
 
 }
